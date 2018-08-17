9fd19e7c42f171625821e045e85cd1c027c3b71d
Mario Pardo <mario.pardo003@gmail.com>
2018-08-17 18:29:21 -0400

    Started moving patrolling ability to C++

---
+++ Source/TestingGroundsFPS/ChooseNextWaypoint.h
@@ -0,0 +1,26 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BehaviorTree/BTTaskNode.h"
+
+#include "ChooseNextWaypoint.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TESTINGGROUNDSFPS_API UChooseNextWaypoint : public UBTTaskNode
+{
+	GENERATED_BODY()
+	
+	
+public:
+protected:
+private:
+
+	 virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
+
+	
+};
