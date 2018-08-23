b3ce85d7c7a3dd37f4678569a7e590248245a74a
Mario Pardo <mario.pardo003@gmail.com>
2018-08-23 16:39:19 -0400

    Changed Patrolling Architecture - Made patrolroute a actorcomponent

--- Source/TestingGroundsFPS/ChooseNextWaypoint.cpp
+++ Source/TestingGroundsFPS/ChooseNextWaypoint.cpp
@@ -1,28 +1,33 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #include "ChooseNextWaypoint.h"
-#include "BehaviorTree/BlackboardComponent.h"
-#include "PatrollingGuard.h"
 #include "AIController.h"
+#include "PatrolRoute.h"
+#include "BehaviorTree/BlackboardComponent.h"
 
 EBTNodeResult::Type UChooseNextWaypoint::ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory)
 {
-	//Get Patrol Points
-	auto AIController = OwnerComp.GetAIOwner();
-	auto ControlledPawn = AIController->GetPawn();
-	auto PatrollingGuard = Cast<APatrollingGuard>(ControlledPawn);
-	auto PatrolPoints = PatrollingGuard->PatrolPointsCPP;
-	
-	//Set Next Waypoint
+	// Get the patrol route
+	auto ControlledPawn = OwnerComp.GetAIOwner()->GetPawn();
+	auto PatrolRoute = ControlledPawn->FindComponentByClass<UPatrolRoute>();
+	if (!ensure(PatrolRoute)) { return EBTNodeResult::Failed; }
+
+	// Warn about empty patrol routes
+	auto PatrolPoints = PatrolRoute->GetPatrolPoints();
+	if (PatrolPoints.Num() == 0)
+	{
+		UE_LOG(LogTemp, Warning, TEXT("A guard is missing patrol points"));
+		return EBTNodeResult::Failed;
+	}
+
+	// Set next waypoint
 	auto BlackboardComp = OwnerComp.GetBlackboardComponent();
 	auto Index = BlackboardComp->GetValueAsInt(IndexKey.SelectedKeyName);
-	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);  //setting the waypoint key at the [index] of patrolpoints
-		//TODO protect against empty patrolroutes
+	BlackboardComp->SetValueAsObject(WaypointKey.SelectedKeyName, PatrolPoints[Index]);
 
-	//Cycle the index
-	auto NextIndex = (Index + 1) % PatrolPoints.Num(); //index + 1 increases it to next waypoint, % arraylength makes it go in a circle for the amt of patrolpoints. if confused on 'circle', watch lecture 237
-	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);  
+	// Cycle the index
+	auto NextIndex = (Index + 1) % PatrolPoints.Num();
+	BlackboardComp->SetValueAsInt(IndexKey.SelectedKeyName, NextIndex);
 
 	return EBTNodeResult::Succeeded;
 }
-
